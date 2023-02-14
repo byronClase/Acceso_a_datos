0be1cb87e0cbcac3741d18d459b36b5a6856da1d
byron <yamahaaerox81@gmail.com>
2023-02-14 01:17:46 +0100

    Acabo de crear un sout en intelliJ

--- src/manual_hibernate/mi_manual_de_hibernate.java
+++ src/manual_hibernate/mi_manual_de_hibernate.java
@@ -3,7 +3,7 @@ package manual_hibernate;
 public class mi_manual_de_hibernate {
 
     public static void main(String[] args) {
-
+        System.out.println("Soy el primer sout del manual");
     }
 
 }
